b072312212556debb9ee39ababdbd9f572a7a40a
anasjaved28 <official.anasjaved@gmail.com>
2026-08-25 10:02:14 +0530

    simon game completed

--- web dev/JS/Simon-Says Game  (Mini-Project)/script.js
+++ web dev/JS/Simon-Says Game  (Mini-Project)/script.js
@@ -2,12 +2,14 @@ let gameSeq = [];
 let userSeq = [];
 let btns = ["red", "blue", "green", "yellow"]; //these are array of classes of buttons
 let isStart = false;
+let highScore = 0;
 let level = 0;
+let h2 = document.querySelector("h2");
 let h3 = document.querySelector("h3");
+let body = document.querySelector("body");
 
 //Detect Keypress to start the Game
-let body = document.querySelector("body");
-body.addEventListener("keydown", function (event) {
+body.addEventListener("keydown", function () {
   if (isStart == false) {
     console.log("Game Started");
     isStart = true;
@@ -16,14 +18,15 @@ body.addEventListener("keydown", function (event) {
 });
 
 function levelUp() {
+  userSeq = [];
   level++;
-  h3.innerText = `Level ${level}`;
+  h2.innerText = `Level ${level}`;
 
-  let randIdx = Math.floor(Math.random() * 5);
+  let randIdx = Math.floor(Math.random() * 4); // idx = [0,1,2,3]
   let randColor = btns[randIdx]; //class of button
   let randBtn = document.querySelector(`.${randColor}`); // button selected via class
-
   gameSeq.push(randColor);
+  console.log(gameSeq);
   gameFlash(randBtn);
 }
 
@@ -35,17 +38,18 @@ function gameFlash(btn) {
 }
 
 let allBtns = document.querySelectorAll(".colorBox");
-for (i of allBtns) {
+for (let i of allBtns) {
   i.addEventListener("click", btnPress);
 }
 
 function btnPress() {
   let btn = this; // this => i
   userFlash(btn);
+
   let userColor = btn.getAttribute("id");
   userSeq.push(userColor);
 
-  checkAns();
+  checkAns(userSeq.length - 1); // passing last index as argument
 }
 
 function userFlash(btn) {
@@ -55,6 +59,33 @@ function userFlash(btn) {
   }, 500);
 }
 
-function checkAns() {
-  console.log("heloow");
+function checkAns(idx) {
+  //comparing the last index color
+  if (userSeq[idx] === gameSeq[idx]) {
+    // continue taking input until both sequence has same length
+    if (userSeq.length == gameSeq.length) {
+      setTimeout(levelUp, 1000);
+    }
+  } else {
+    h2.innerHTML = `Game Over! Your score was <b>${level - 1}</b> <br> Press any key to start`;
+
+    body.style.backgroundColor = "red";
+
+    setTimeout(function () {
+      body.style.backgroundColor = "white";
+    }, 1000);
+
+    if (level >= highScore) {
+      highScore = level;
+    }
+    h3.innerHTML = `High Score: ${highScore - 1}`;
+    reset(); //reset when else is executed
+  }
+}
+
+function reset() {
+  isStart = false;
+  gameSeq = [];
+  userSeq = [];
+  level = 0;
 }
