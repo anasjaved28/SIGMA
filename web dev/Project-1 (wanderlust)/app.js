cf578727be9481e212f2978c6b90fa8307912e3b
anasjaved28 <official.anasjaved@gmail.com>
2026-09-22 23:59:34 +0530

    a

--- web dev/Project-1 (wanderlust)/app.js
+++ web dev/Project-1 (wanderlust)/app.js
@@ -5,11 +5,14 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js"); //requiring model
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -36,19 +39,12 @@ app.get("/", (req, res) => {
 //Index Route
 app.get("/listings", async (req, res) => {
   const allListings = await Listing.find({});
-  res.render("index.ejs", { allListings });
+  res.render("listings/index.ejs", { allListings });
 });
 
 //New Route
 app.get("/listings/new", (req, res) => {
-  res.render("new.ejs");
-});
-
-//Show Route
-app.get("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("show.ejs", { listing });
+  res.render("listings/new.ejs");
 });
 
 //Create Route
@@ -58,11 +54,18 @@ app.post("/listings", async (req, res) => {
   res.redirect("/listings");
 });
 
+//Show Route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
 //Edit Route
 app.get("/listings/:id/edit", async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
-  res.render("edit.ejs", { listing });
+  res.render("listings/edit.ejs", { listing });
 });
 
 //Update Route
